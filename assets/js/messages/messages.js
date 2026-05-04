7dfa34b47a31afe5ff680a66d4a3e7bf4d42b1e1
TheRealStrangeFox <jabakhachidze.jj.com>
2026-05-04 20:44:37 +0400

    change hero part

--- assets/js/messages/messages.js
+++ assets/js/messages/messages.js
@@ -11,7 +11,11 @@ var translations = {
     write_message: "Write message here",
     have_question: "Do you have any question?",
     call_us: "Call us for imiditate support",
-    send_email: "Send us email for any kind of inquiry"
+    send_email: "Send us email for any kind of inquiry",
+    hero_headline: "One system for all devices",
+    hero_summary: "We are adding cashless payments to self-service machines. quickly, easily and efficiently",
+    start_now: "Get Started Now",
+
   },
   ge: {
     home: "მთავარი",
@@ -24,6 +28,9 @@ var translations = {
     write_message: "დაწერეთ შეტყობინება აქ",
     have_question: "გაქვთ რაიმე შეკითხვა?",
     call_us: "დაგვიკავშირდით დაუყოვნებელი მხარდაჭერისთვის",
-    send_email: "მოგვწერეთ ელფოსტაზე ნებისმიერი შეკითხვისთვის"
+    send_email: "მოგვწერეთ ელფოსტაზე ნებისმიერი შეკითხვისთვის",
+    hero_headline: "ერთი სისტემა ყველა აპარატისთის",
+    hero_summary: "ვამატებთ საბარათე და ციფრულ გადახდებს თვითმომსახურების აპარატებზე, სწრაფად, მარტივად და ეფექტურად",
+    start_now: "დაიწყე ახლა",
   }
 };
